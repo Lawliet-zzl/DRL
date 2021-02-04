ea21446b35a4902c0c86fedf78c06ea3f0797846
Lawliet-zzl <78414128+Lawliet-zzl@users.noreply.github.com>
2021-02-04 10:21:32 +0800

    Add files via upload

---
+++ code_RCL/demo.sh
@@ -0,0 +1,10 @@
+DATASET='CIFAR10' # ('CIFAR10', 'CIFAR100')
+MODEL='ResNet18' # ('ResNet18' 'MobileNetV2' 'SENet' 'DPN26')
+ALPHA='0.1'
+EPS='0.001' # (0.001, 0.0001)
+NAME='0'
+
+python main_baseline.py --model ${MODEL} --dataset ${DATASET} --name=${NAME}\
+
+python main_RCL.py --pretrained-model ${MODEL} --model ${MODEL} --dataset ${DATASET} --name=${NAME}\
+ --eps ${EPS} --alpha ${ALPHA}\
